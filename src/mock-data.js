59ab1d7071e43492172b8a9e835b18a4e99f470e
hpurba <hikaru.purba@gmail.com>
2020-03-17 18:15:47 -0600

    Made changes to the mock-data.js for all the cat pictures

--- src/mock-data.js
+++ src/mock-data.js
@@ -1,357 +1,205 @@
-let mock = [{
-    id: 1,
-    name: "Egg Salad",
-    price: "$5.62",
-    country: "United States",
-    image: 'egg-salad.jpg'
+let mock = [
+// BEAUTIFUL
+  {
+    id: 1,                      // id
+    name: "Alex and Ron",               // name
+    gender: "male",             // price
+    category: "beautiful",  // country
+    image: 'beautifulCat01.jpg' // image
   },
   {
     id: 2,
-    name: "Sweet Potato",
-    price: "$2.41",
-    country: "Brazil",
-    image: "sweet-potato.jpg"
+    name: "Jay",
+    gender: "female",
+    category: "beautiful",
+    image: "beautifulCat02.jpg"
   },
   {
     id: 3,
-    name: "Foccacia Bread",
-    price: "$5.92",
-    country: "United States",
-    image: 'focaccia.jpg'
+    name: "Hikaru",
+    gender: "male",
+    category: "beautiful",
+    image: 'beautifulCat03.jpg'
   },
   {
     id: 4,
-    name: "Canned Lotus Rootlets",
-    price: "$3.58",
-    country: "United States",
-    image: 'lotus-rootlets.jpg'
+    name: "John",
+    gender: "male",
+    category: "beautiful",
+    image: 'beautifulCat04.jpg'
   },
   {
     id: 5,
-    name: "Frilled Toothpicks",
-    price: "$2.47",
-    country: "Canada",
-    image: 'toothpicks.jpg'
+    name: "Rei",
+    gender: "male",
+    category: "beautiful",
+    image: 'beautifulCat05.jpg'
 
   },
   {
     id: 6,
-    name: "Rubbed Sage",
-    price: "$8.77",
-    country: "Brazil",
-    image: 'sage-rubbed.jpg'
+    name: "Markus",
+    gender: "male",
+    category: "beautiful",
+    image: 'beautifulCat06.jpg'
   },
   {
     id: 7,
-    name: "Langers Mango Nectar",
-    price: "$9.24",
-    country: "Brazil",
-    image: 'mango-nectar.jpg'
+    name: "Steve",
+    gender: "male",
+    category: "beautiful",
+    image: 'beautifulCat07.jpg'
   },
   {
     id: 8,
-    name: "Cucumber",
-    price: "$3.02",
-    country: "Canada",
-    image: 'cucumber.jpg'
+    name: "Will",
+    gender: "male",
+    category: "beautiful",
+    image: 'beautifulCat08.jpg'
   },
   {
     id: 9,
-    name: "Fava Beans",
-    price: "$4.76",
-    country: "Brazil",
-    image: 'fava-beans.jpg'
-
+    name: "Emily and Jane",
+    gender: "female",
+    category: "beautiful",
+    image: 'beautifulCat09.jpg'
   },
+
+
+// COOL
   {
-    id: 10,
-    name: "Coconut",
-    price: "$2.50",
-    country: "Brazil",
-    image: 'coconut.jpg'
+    id: 10,                      // id
+    name: "Brian",               // name
+    gender: "male",             // price
+    category: "cool",  // country
+    image: 'coolCat01.jpg' // image
   },
   {
     id: 11,
-    name: "Kettle Potato Chips",
-    price: "$6.70",
-    country: "United States",
-    image: "potato-chips.jpg"
+    name: "Shawn",
+    gender: "male",
+    category: "cool",
+    image: "coolCat02.jpg"
   },
   {
     id: 12,
-    name: "Flour - Rye",
-    price: "$1.14",
-    country: "Canada",
-    image: "rye-flour.jpg"
+    name: "Suzy",
+    gender: "female",
+    category: "cool",
+    image: 'coolCat03.jpg'
   },
   {
     id: 13,
-    name: "Feta Chees",
-    price: "$6.92",
-    country: "Brazil",
-    image: "feta-cheese.jpg"
+    name: "Annie",
+    gender: "female",
+    category: "cool",
+    image: 'coolCat04.jpg'
   },
   {
     id: 14,
-    name: "Cheddar Cheese",
-    price: "$5.36",
-    country: "Mexico",
-    image: "cheddar-cheese.jpg"
+    name: "Calvin",
+    gender: "male",
+    category: "cool",
+    image: 'coolCat05.jpg'
+
   },
   {
     id: 15,
-    name: "Capers",
-    price: "$4.18",
-    country: "United States",
-    image: "capers.jpg"
+    name: "Ken",
+    gender: "male",
+    category: "cool",
+    image: 'coolCat06.jpg'
   },
   {
     id: 16,
-    name: "Baguette",
-    price: "$9.18",
-    country: "Mexico",
-    image: "baguette.jpg"
+    name: "Ryan",
+    gender: "male",
+    category: "cool",
+    image: 'coolCat07.jpg'
   },
+
+
+// CUTE
   {
-    id: 17,
-    name: "Lasagna Noodles",
-    price: "$4.73",
-    country: "Brazil",
-    image: "lasagna-pasta.jpg"
+    id: 17,                      // id
+    name: "Ean",               // name
+    gender: "male",             // price
+    category: "cute",  // country
+    image: 'cuteCat01.jpg' // image
   },
   {
     id: 18,
-    name: "Pied De Vents Cheese",
-    price: "$5.16",
-    country: "Canada",
-    image: "pied-de-vents.jpg"
+    name: "Jared",
+    gender: "male",
+    category: "cute",
+    image: "cuteCat02.jpg"
   },
   {
     id: 19,
-    name: "Sweet Potato Crackers",
-    price: "$6.81",
-    country: "United States",
-    image: "crackers.jpg"
+    name: "Bryan",
+    gender: "male",
+    category: "cute",
+    image: 'cuteCat03.jpg'
   },
   {
     id: 20,
-    name: "Mushrooms",
-    price: "$8.62",
-    country: "United States",
-    image: "mushrooms.jpg"
+    name: "Bella",
+    gender: "female",
+    category: "cute",
+    image: 'cuteCat04.jpg'
   },
   {
     id: 21,
-    name: "Jerusalem Artichoke",
-    price: "$3.16",
-    country: "United States",
-    image: "jerusalem-artichoke.jpg"
+    name: "Isaac",
+    gender: "male",
+    category: "cute",
+    image: 'cuteCat05.jpg'
+
   },
   {
     id: 22,
-    name: "Leather Leaf Fern",
-    price: "$9.41",
-    country: "Brazil",
-    image: "leather-leaf.jpg"
+    name: "Shae",
+    gender: "female",
+    category: "cute",
+    image: 'cuteCat06.jpg'
   },
   {
     id: 23,
-    name: "Dried Apricots",
-    price: "$7.42",
-    country: "Brazil",
-    image: "dried-apricots.jpg"
+    name: "Carson",
+    gender: "male",
+    category: "cute",
+    image: 'cuteCat07.jpg'
   },
   {
     id: 24,
-    name: "Navel Orange",
-    price: "$3.53",
-    country: "Brazil",
-    image: "navel-orange.jpg"
+    name: "Lauren",
+    gender: "female",
+    category: "cute",
+    image: 'cuteCat08.jpg'
   },
   {
     id: 25,
-    name: "Sunflower Oil",
-    price: "$1.90",
-    country: "Mexico",
-    image: "sunflower-oil.jpg"
+    name: "Levi",
+    gender: "male",
+    category: "cute",
+    image: 'cuteCat09.jpg'
   },
+
+
+// FUN
   {
-    id: 26,
-    name: "Black Turtle Beans",
-    price: "$4.67",
-    country: "Canada",
-    image: "turtle-beans.jpg"
+    id: 26,                      // id
+    name: "Ken",               // name
+    gender: "male",             // price
+    category: "fun",  // country
+    image: 'funCat01.jpg' // image
   },
   {
     id: 27,
-    name: "Bananas",
-    price: "$2.64",
-    country: "Canada",
-    image: "bananas.jpg"
-
-  },
-  {
-    id: 28,
-    name: "Salami",
-    price: "$8.72",
-    country: "Brazil",
-    image: "salami.jpg"
-  },
-  {
-    id: 29,
-    name: "Proscuitto",
-    price: "$9.86",
-    country: "Brazil",
-    image: "proscuitto.jpg"
-  },
-  {
-    id: 30,
-    name: "Cranberries",
-    price: "$5.85",
-    country: "United States",
-    image: "cranberries.jpg"
-
-  },
-  {
-    id: 31,
-    name: "Canteloupe",
-    price: "$4.49",
-    country: "United States",
-    image: "canteloupe.jpg"
-  },
-  {
-    id: 32,
-    name: "Lemon",
-    price: "$2.8Lemon5",
-    country: "United States",
-    image: "lemon.jpg"
-  },
-  {
-    id: 33,
-    name: "Pesto",
-    price: "$2.67",
-    country: "Brazil",
-    image: "pesto.jpg"
-  },
-  {
-    id: 34,
-    name: "Grapefruit Juice",
-    price: "$4.88",
-    country: "Brazil",
-    image: "grapefruit-juice.jpg"
-
-  },
-  {
-    id: 35,
-    name: "Curry Paste",
-    price: "$2.02",
-    country: "Brazil",
-    image: "curry-paste.jpg"
-  },
-  {
-    id: 36,
-    name: "Avocado Oil",
-    price: "$4.45",
-    country: "Canada",
-    image: "avocado-oil.jpg"
-  },
-  {
-    id: 37,
-    name: "Queso Fresco",
-    price: "$9.24",
-    country: "Canada",
-    image: "queso-fresco.jpg"
-  },
-  {
-    id: 38,
-    name: "Key Lime Pie",
-    price: "$5.32",
-    country: "Mexico",
-    image: "key-lime-pie.jpg"
-  },
-  {
-    id: 39,
-    name: "Granny Smith Apple",
-    price: "$2.12",
-    country: "United States",
-    image: "granny-smith.jpg"
-  },
-  {
-    id: 40,
-    name: "Morning Glory Muffin Mix",
-    price: "$9.22",
-    country: "Brazil",
-    image: "muffin-mix.jpg"
-  },
-  {
-    id: 41,
-    name: "Heavy Whipping Cream",
-    price: "$8.78",
-    country: "Brazil",
-    image: 'heavy-cream.jpg'
-  },
-  {
-    id: 42,
-    name: "Red Bell Pepper",
-    price: "$9.03",
-    country: "Brazil",
-    image: 'red-bell-pepper.jpg'
-  },
-  {
-    id: 43,
-    name: "Perrier",
-    price: "$9.36",
-    country: "United States",
-    image: 'perrier.jpg'
-  },
-  {
-    id: 44,
-    name: "Yukon Gold Potatoes",
-    price: "$8.76",
-    country: "Mexico",
-    image: 'yukon-potatoes.jpg'
-  },
-  {
-    id: 45,
-    name: "Salmon Fillets",
-    price: "$6.21",
-    country: "United States",
-    image: 'salmon-fillets.jpg'
-  },
-  {
-    id: 46,
-    name: "Tilapia Fillets",
-    price: "$4.37",
-    country: "Mexico",
-    image: 'tilapia.jpg'
-  },
-  {
-    id: 47,
-    name: "Tomato Pesto",
-    price: "$9.70",
-    country: "Brazil",
-    image: 'tomato-pesto.jpg'
-  },
-  {
-    id: 48,
-    name: "Oil - Olive Bertolli",
-    price: "$8.92",
-    country: "Brazil",
-    image: 'olive-oil.jpg'
-  },
-  {
-    id: 49,
-    name: "Tear Drop Yellow Tomatoes",
-    price: "$3.32",
-    country: "Mexico",
-    image: 'yellow-tomatoes.jpg'
-  },
-  {
-    id: 50,
-    name: "Smoked Paprika",
-    price: "$8.31",
-    country: "Brazil",
-    image: 'smoked-paprika.jpg'
+    name: "Daniel",
+    gender: "male",
+    category: "fun",
+    image: "funCat02.jpg"
   }
 ]
 
